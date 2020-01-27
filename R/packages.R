77b7837e525fb6e18fc970865ac413c0fd288663
Pascal PERNOT <pascal.pernot@u-psud.fr>
2020-01-27 14:24:18 +0100

    Adapt to publication on shinyapps.io

--- R/packages.R
+++ R/packages.R
@@ -2,7 +2,7 @@
 Sys.setlocale(category = "LC_NUMERIC", locale = "C")
 
 options(
-  shiny.maxRequestSize = 20 * 1024^2,
+  # shiny.maxRequestSize = 20 * 1024^2,
   width = 60,
   warn = 0
 )
@@ -10,18 +10,35 @@ options(
 # options(shiny.json.digits=32)
 
 # CRAN Libraries ####
-libs <- c(
-  "devtools","Hmisc", "rlist", "boot",
-  "shiny", "shinyBS", "shinycssloaders", "shinyFiles",
-  "DT", "tools", "inlmisc", "distillery","ErrViewLib"
-)
-for (lib in libs) {
-  if (!require(lib, character.only = TRUE, quietly = TRUE)) {
-    install.packages(
-      lib,
-      dependencies = TRUE,
-      repos = "https://cran.univ-paris1.fr"
-    )
-    library(lib, character.only = TRUE, quietly = TRUE)
-  }
-}
+# libs <- c(
+#   "devtools", "Hmisc", "rlist", "boot",
+#   "shiny", "shinyFiles", #"shinyBS", "shinycssloaders",
+#   "DT", "tools", "inlmisc", "distillery"
+# )
+# for (lib in libs) {
+#   if (!require(lib, character.only = TRUE, quietly = TRUE)) {
+#     install.packages(
+#       lib,
+#       dependencies = TRUE #,
+#       # repos = "https://cran.univ-paris1.fr"
+#     )
+#     library(lib, character.only = TRUE, quietly = TRUE)
+#   }
+# }
+# lib = "ErrViewLib"
+# if(!require(lib,character.only = TRUE))
+#   devtools::install_github(paste0("ppernot/",lib))
+# library(lib,character.only = TRUE)
+
+# For Shinyio.apps
+library(devtools)
+library(Hmisc)
+library(rlist)
+library(boot)
+library(shiny)
+library(shinyFiles)
+library(DT)
+library(tools)
+library(inlmisc)
+library(distillery)
+library(ErrViewLib)
--- ui_files/about.R
+++ ui_files/about.R
@@ -2,13 +2,13 @@ sidebarPanel(
   width = sideWidth,
   h5("Author      : P. Pernot"),
   h5("Affiliation : CNRS"),
-  h5("Version     : 0.0.0"),
-  h5("Date        : 2019/01/14"),
+  h5("Version     : 0.1"),
+  h5("Date        : 2020/01/27"),
   hr( style="border-color: #666;"),
-  a(href="https://github.com/ppernot/ReactorUI","How to cite..."),
+  a(href="https://github.com/ppernot/ErrView","How to cite..."),
   br(),
-  a(href="https://github.com/ppernot/ReactorUI","code@github"),
+  a(href="https://github.com/ppernot/ErrView","code@github"),
   br(),
-  a(href="https://github.com/ppernot/ReactorUI/issues",
-    "Bugs report, Features request")
+  a(href="https://github.com/ppernot/ErrView/issues",
+    "Bug report, Feature request")
 )
